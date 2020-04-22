dc395272faa77eb6ee4ce02df4f7f0a145fdbb54
Lucas Diez <lucasgdiez@gmail.com>
2020-04-22 13:52:02 -0300

    adding necessary functions to make base ui work

--- src/modules/AppRoot.tsx
+++ src/modules/AppRoot.tsx
@@ -3,38 +3,48 @@ import { Provider } from 'react-redux';
 import { ConnectedRouter } from 'connected-react-router';
 import { Switch, Route } from 'react-router';
 
+import { Client as Styletron } from 'styletron-engine-atomic';
+import { Provider as StyletronProvider } from 'styletron-react';
+import { LightTheme, BaseProvider } from 'baseui';
+
 import { store, history } from './state-mgmt/store';
 
-const Login = lazy(() => import('./views/Login'));
+const engine = new Styletron();
+
+const Login = lazy(() => import('./views/login/Login'));
 const TodoList = lazy(() => import('./views/TodoList'));
 
 export default class App extends React.Component {
   public render() {
     return (
-      <Provider store={store}>
-        <ConnectedRouter history={history}>
-          <Switch>
-            <Route
-              exact={true}
-              path="/"
-              render={() => (
-                <Suspense fallback={<p>loading</p>}>
-                  <Login />
-                </Suspense>
-              )}
-            />
-            <Route
-              exact={true}
-              path="/todo-list"
-              render={() => (
-                <Suspense fallback={<p>loading</p>}>
-                  <TodoList />
-                </Suspense>
-              )}
-            />
-          </Switch>
-        </ConnectedRouter>
-      </Provider>
+      <StyletronProvider value={engine}>
+        <BaseProvider theme={LightTheme}>
+          <Provider store={store}>
+            <ConnectedRouter history={history}>
+              <Switch>
+                <Route
+                  exact={true}
+                  path="/"
+                  render={() => (
+                    <Suspense fallback={<p>loading</p>}>
+                      <Login login={(username, password) => undefined} />
+                    </Suspense>
+                  )}
+                />
+                <Route
+                  exact={true}
+                  path="/todo-list"
+                  render={() => (
+                    <Suspense fallback={<p>loading</p>}>
+                      <TodoList />
+                    </Suspense>
+                  )}
+                />
+              </Switch>
+            </ConnectedRouter>
+          </Provider>
+        </BaseProvider>
+      </StyletronProvider>
     );
   }
 }
